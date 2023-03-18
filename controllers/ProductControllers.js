f4cf92e2e4a49c16e230f17216061afba88ac443
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-03-18 12:54:37 +0300

    feta: add sort, total products, inc views

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -1,130 +1,146 @@
-import {ProductModel} from "../models/Product.js";
+import { ProductModel } from "../models/Product.js";
 import chalk from "chalk";
 
 export const createProduct = async (req, res) => {
-	try {
-		const doc = new ProductModel({
-			...req.body
-		})
-		const newProduct = await doc.save()
+  try {
+    const doc = new ProductModel({
+      ...req.body,
+    });
+    const newProduct = await doc.save();
 
-		res.status(201).json(newProduct)
-
-	} catch (e) {
-		console.log(chalk.red(e.message))
-		res.status(500).json({
-			message: "Не удалось создать пост"
-		})
-	}
-}
+    res.status(201).json(newProduct);
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    res.status(500).json({
+      message: "Не удалось создать пост",
+    });
+  }
+};
 
 export const getProducts = async (req, res) => {
-	try {
-		const list = await ProductModel.find()
+  try {
+    const { article, year, mark } = req.query;
 
-		const page = parseInt(req.query.page)
-		const limit = parseInt(req.query.limit)
+    const queries = {};
+    if (article) {
+      queries.article = article;
+    }
+    if (year) {
+      queries.year = year;
+    }
+    if (mark) {
+      queries.mark = mark;
+    }
 
-		const startIndex = (page - 1) * limit;
-		const endIndex = page * limit;
-		const totalPages = Math.round(list.length / limit)
+    const list = await ProductModel.find(queries).sort({ createdAt: -1 });
 
-		const results = {};
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
-		results.totalPages = totalPages
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const totalPages = Math.round(list.length / limit);
 
-		if (endIndex < list.length) {
-			results.next = {
-				page: page + 1,
-				limit: limit,
-			};
-		}
+    const results = {};
+    results.totalProducts = list.length;
 
-		if (startIndex > 0) {
-			results.previous = {
-				page: page - 1,
-				limit: limit
-			}
-		}
-		// const list = await ProductModel.find()
-		results.results = list.slice(startIndex, endIndex)
-		if (list) {
-			res.status(200).json(results)
-		}
+    results.totalPages = totalPages;
 
-	} catch (e) {
-		res.status(500).json({
-			message: "Не удалось получить товары"
-		})
-	}
-}
+    if (endIndex < list.length) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    // const list = await ProductModel.find()
+    results.results = list.slice(startIndex, endIndex);
+    if (list) {
+      res.status(200).json(results);
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Не удалось получить товары",
+    });
+  }
+};
 
 export const deleteProduct = async (req, res) => {
-	try {
-		const {id} = req.body
-		await ProductModel.findByIdAndDelete(id)
-		res.json({
-			success: true,
-			message: 'Продукт удален'
-		})
-
-	} catch (e) {
-		console.log(chalk.red(e.message))
-		res.status(500).json({
-			message: "Не удалось удалить товары"
-		})
-	}
-}
+  try {
+    const { id } = req.body;
+    await ProductModel.findByIdAndDelete(id);
+    res.json({
+      success: true,
+      message: "Продукт удален",
+    });
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    res.status(500).json({
+      message: "Не удалось удалить товары",
+    });
+  }
+};
 
 export const getOneProduct = async (req, res) => {
-	try {
-		const {id} = req.params
-		const product = await ProductModel.findById(id)
-		if (product) {
-			res.status(200).send(product)
-		}
-
-	} catch (e) {
-		res.status(500).json({
-			message: 'Не удалось найти продукт'
-		})
-	}
-}
-
+  try {
+    const { id } = req.params;
+    const product = await ProductModel.findByIdAndUpdate(id, {
+      $inc: { views: 1 },
+    });
+    if (product) {
+      res.status(200).send(product);
+    }
+  } catch (e) {
+    res.status(500).json({
+      message: "Не удалось найти продукт",
+    });
+  }
+};
 
 export const updateProduct = async (req, res) => {
-	try {
-		const {id} = req.params
-		await ProductModel.updateOne({
-			_id: id,
-		}, {
-			...req.body
-		})
+  try {
+    const { id } = req.params;
+    await ProductModel.updateOne(
+      {
+        _id: id,
+      },
+      {
+        ...req.body,
+      }
+    );
 
-		res.status(200).json({
-			success: true,
-			message: 'Продукт обновлен'
-		})
-	} catch (e) {
-		res.status(500).json({
-			message: 'Не удалось обновить продукт'
-		})
-	}
-}
+    res.status(200).json({
+      success: true,
+      message: "Продукт обновлен",
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Не удалось обновить продукт",
+    });
+  }
+};
 
 export const findProducts = async (req, res) => {
-	try {
-		const { search, category, model, year } = req.query;
-		console.log(category, model, year, search)
-		// const list = await ProductModel.find({$or: [{mark: model}, {product: search}]})
-		const list = await ProductModel.find({mark: model, product: search, year: year})
-		console.log(list)
-		res.status(200).send(list)
-
-
-	} catch (e) {
-		res.status(500).json({
-			message: 'Не найти продукт'
-		})
-	}
-}
+  try {
+    const { search, category, model, year } = req.query;
+    console.log(category, model, year, search);
+    // const list = await ProductModel.find({$or: [{mark: model}, {product: search}]})
+    const list = await ProductModel.find({
+      mark: model,
+      product: search,
+      year: year,
+    });
+    console.log(list);
+    res.status(200).send(list);
+  } catch (e) {
+    res.status(500).json({
+      message: "Не найти продукт",
+    });
+  }
+};
