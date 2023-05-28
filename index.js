a225a681db2d6b71e204a274728d80f6f3fc8999
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-05-29 00:50:36 +0300

    feat: add mkdirSync uploads

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 import express from "express";
 import chalk from "chalk";
 import cors from "cors";
+import fs from "fs";
 import config from "config";
 import mongoose from "mongoose";
 import multer from "multer";
-import { auth } from "./middleware/auth.middleware.js";
+import {auth} from "./middleware/auth.middleware.js";
 import {
-  getMe,
-  login,
-  refreshToken,
-  signUp,
+	getMe,
+	login,
+	refreshToken,
+	signUp,
 } from "./controllers/UserControlles.js";
 import {
-  createProduct,
-  deleteProduct,
-  getOneProduct,
-  getProducts,
-  updateProduct,
+	createProduct,
+	deleteProduct,
+	getOneProduct,
+	getProducts,
+	updateProduct,
 } from "./controllers/ProductControllers.js";
 import {
-  getAutosInfo,
-  getOptionsInfo,
-  getPartsList,
+	getAutosInfo,
+	getOptionsInfo,
+	getPartsList,
 } from "./controllers/autosInfo.js";
-import { mainRouter } from "./routes/main.js";
+import {mainRouter} from "./routes/main.js";
 
 const PORT = config.get("port") ?? 8888;
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
 // папка со статичными файлами
@@ -38,17 +39,20 @@ app.use("/uploads", express.static("uploads"));
 
 // store multer
 const storage = multer.diskStorage({
-  // когда будет загружаться любой файл,   будет работать функция которая вернет путь файла
-  destination: (_, __, cb) => {
-    cb(null, "uploads");
-  },
-  // перед тем как сохранить функция укажет как называеться файл
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
+	// когда будет загружаться любой файл,   будет работать функция которая вернет путь файла
+	destination: (_, __, cb) => {
+		if (fs.existsSync("uploads")) {
+			fs.mkdirSync("uploads")
+		}
+		cb(null, "uploads");
+	},
+	// перед тем как сохранить функция укажет как называеться файл
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 
-const upload = multer({ storage });
+const upload = multer({storage});
 // запрос на загрузку
 // app.post('/upload', upload.single('image'), (req, res) => {
 //   res.json({
@@ -64,13 +68,13 @@ app.post("/admin/verify", getMe);
 
 // загрузка нескольких файлов
 app.post("/upload", upload.array("image"), (req, res) => {
-  const file = req.files;
-  const originalNames = file.map((file) => `/uploads/${file.originalname}`);
+	const file = req.files;
+	const originalNames = file.map((file) => `/uploads/${file.originalname}`);
 
-  res.send(originalNames);
-  // res.json({
-  //   url: `/uploads/${req.file.originalname}`
-  // })
+	res.send(originalNames);
+	// res.json({
+	//   url: `/uploads/${req.file.originalname}`
+	// })
 });
 
 // работа с товарами
@@ -89,19 +93,19 @@ app.get("/getPartsList", getPartsList);
 app.use("/", mainRouter);
 
 async function start() {
-  try {
-    // mongoose.connection.once("open", () => {
-    //   initDatabase()
-    // });
-    await mongoose.connect(config.get("mongoUrl"));
-    console.log(chalk.green("DB connected"));
-    app.listen(PORT, () => {
-      console.log(chalk.green(`Server start om port ${PORT}`));
-    });
-  } catch (e) {
-    console.log(chalk.red(e.message));
-    process.exit(1);
-  }
+	try {
+		// mongoose.connection.once("open", () => {
+		//   initDatabase()
+		// });
+		await mongoose.connect(config.get("mongoUrl"));
+		console.log(chalk.green("DB connected"));
+		app.listen(PORT, () => {
+			console.log(chalk.green(`Server start om port ${PORT}`));
+		});
+	} catch (e) {
+		console.log(chalk.red(e.message));
+		process.exit(1);
+	}
 }
 
 start();
