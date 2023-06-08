f6a417ac0f80888c0e9c214a7d599985a240d2d4
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-08 12:57:55 +0300

    feat: add cors

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+	origin: 'https://back-autoparts.herokuapp.com'
+}));
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
