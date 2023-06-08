3e1d7189216a4b0311cbdf407cedc7cbf5986af3
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-08 12:54:42 +0300

    feat: add Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors());
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
+
 // папка со статичными файлами
 app.use("/uploads", express.static("uploads"));
 
