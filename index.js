23673ee7f0d761d53beab91b983bd95bfc205de1
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-10-05 15:37:46 +0300

    test

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const corsOptions = {
     "http://xn80aedi.vh121.hosterby.com",
     "http://xn--80aedi4aemb7a3h.xn--90ais",
     "https://back-autoparts.onrender.com",
+    "https://back-autoparts.onrender.com",
   ],
   credentials: true,
 };
@@ -55,6 +56,7 @@ app.use((req, res, next) => {
     "http://localhost:5173",
     "http://xn80aedi.vh121.hosterby.com",
     "http://xn--80aedi4aemb7a3h.xn--90ais/",
+    "https://back-autoparts.onrender.com",
     "https://back-autoparts.onrender.com"
   );
   next();
